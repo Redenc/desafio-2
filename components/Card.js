f929a1b771f3244436e9842a30b60054f5bf8386
Redenc <ferragudagustin2@gmail.com>
2022-07-13 01:37:43 -0300

    Se creo el componen "Card"

---
+++ components/Card.js
@@ -0,0 +1,10 @@
+const Card = () => 
+<article className="container">
+    <img src="https://comunidad.retorn.com/wp-content/uploads/cache/2018/09/gatitos/3008811440.jpg"></img>
+<h2>Michifus</h2>
+<p>Miau miau miau miau.......
+</p>
+<button>ingresar</button>
+</article>
+
+export default Card
